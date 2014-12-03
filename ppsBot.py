3d16d02943bcc1e231ee0ea0785343a8a5e19bd4
Sivasubramanyam A <astronomersiva@users.noreply.github.com>
2014-12-03 15:49:24 +0530

    Create ppsBot.py

---
+++ ppsBot.py
@@ -0,0 +1,34 @@
+from facepy import GraphAPI
+import json
+import requests
+import os
+
+def downloadFile(url, fileName):
+    r = requests.get(url, stream = True)
+    with open(fileName, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk:
+                f.write(chunk)
+                f.flush()
+
+
+
+groupId = "714685238558448"
+accessToken = "###"
+
+graph = GraphAPI(accessToken)
+pages = graph.get(groupId + "/files", page = True, retry = 5, limit = 2000)
+counter = 0
+
+os.mkdir('fbPythonGroup')
+os.chdir(os.getcwd() + '/fbPythonGroup')
+
+for page in pages:
+    for post in page['data']:
+        url = post['download_link']
+        #fileName is extracted from download url of the following type
+        #https://www.facebook.com/download/837214882985735/scipy-ref-0.14.0.pdf
+        fileName = url[50:]
+        print "fetching " + url
+        downloadFile(url, fileName)
+
